8153bd865b372a75d993d677040c87a037749af4
Gürhan Mermer <gmermer@emakina.ch>
2018-07-23 14:20:11 +0300

    flash booking

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -13,7 +13,8 @@ class App extends React.Component {
         	schedule: null,
             current: null,
             next: null,
-            isBooking: false
+            isBooking: false,
+            isPosting: false
         };
     }
 
@@ -42,7 +43,8 @@ class App extends React.Component {
         });
 
         // Refresh at an interval
-        this.interval = setInterval(() => getCurrentEvent(), this.props.refresh);
+        this.interval = setInterval(() =>
+            getCurrentEvent(), this.props.refresh);
 	}
 
     componentWillUnmount () {
@@ -51,6 +53,32 @@ class App extends React.Component {
 
     setBooking (state) {
         this.setState({ isBooking: state });
+
+        if (state) {
+            this.timeout = setTimeout(() =>
+                this.setState({ isBooking: false }), this.props.timeout);
+        } else if (this.timeout) {
+            clearTimeout(this.timeout);
+        }
+    }
+
+    setPosting (state) {
+        this.setState({ isPosting: state });
+    }
+
+    sendRequest (mins) {
+        let room = this.props.room,
+            schedule = this.props.services.schedule;
+
+        this.setPosting(true);
+
+        schedule.sendBookingRequest(room, mins).then((success) => {
+            schedule.getEvents(room).then((schedule) => {
+                this.setState({ schedule: schedule });
+            });
+
+            this.setPosting(false);
+        });
     }
 
     render () {
@@ -58,7 +86,8 @@ class App extends React.Component {
             schedule = this.state.schedule,
     		current = this.state.current,
             next = this.state.next,
-            isBooking = this.state.isBooking;
+            isBooking = this.state.isBooking,
+            isPosting = this.state.isPosting;
 
 		return (!schedule) ? null : (
 			<main className={(current ? 'busy' : '')}>
@@ -66,12 +95,15 @@ class App extends React.Component {
                     room={room}
                     current={current}
                     isBooking={isBooking}
+                    isPosting={isPosting}
                     setBooking={this.setBooking.bind(this)} />
 				<Status
                     next={next}
                     current={current}
                     isBooking={isBooking}
-                    setBooking={this.setBooking.bind(this)} />
+                    isPosting={isPosting}
+                    setBooking={this.setBooking.bind(this)}
+                    sendRequest={this.sendRequest.bind(this)} />
 				<Timeline schedule={schedule} />
 			</main>
 		);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 
 ReactDOM.render(
-	<App room={5} refresh={1000} />,
+	<App room={1} refresh={5000} timeout={15000} />,
 	document.getElementById('app')
 );
