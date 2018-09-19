4332dca7e58f1363e31360e81a7841482c138e0a
Gürhan Mermer <gmermer@emakina.ch>
2018-09-19 09:56:47 +0300

    allow setting id/title on build

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import App from './components/app';
 
 ReactDOM.render(
 	<App
-		id={1}
-		title="Saturn"
+		id={process.env.id || 1}
+		title={process.env.title || "Meeting Room"}
 		refresh={1000}
 		timeout={15000} />,
 	document.getElementById('app')
