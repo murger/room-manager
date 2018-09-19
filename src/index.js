6100eb0c384790f68e328d19ee339f36230b3d35
Gürhan Mermer <gmermer@emakina.ch>
2018-09-19 14:03:16 +0300

    id/title set via query string

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
+let params = new URLSearchParams(window.location.search);
+
 ReactDOM.render(
 	<App
-		id={process.env.id || 1}
-		title={process.env.title || "Meeting Room"}
+		id={params.get('id') || 1}
+		title={params.get('title') || "Meeting Room"}
 		refresh={1000}
 		timeout={15000} />,
 	document.getElementById('app')
