2c0f1e8d44d1f0d3eb03105b3c58889ef156911d
Gürhan Mermer <gmermer@emakina.ch>
2018-09-19 17:08:26 +0300

    set api in services

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 import ScheduleService from './schedule';
 
+let api = 'https://meeting-room-api-emakinatr.herokuapp.com/api/v1';
+
 export default {
-	schedule: new ScheduleService()
+	schedule: new ScheduleService(api)
 };
